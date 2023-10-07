fddf5cbb700888d98f6f49ad3c4e4040e2957a9a
Danny Sears <127002102+Danny-sears@users.noreply.github.com>
2023-10-07 01:16:31 -0500

    Added cases for other instruction formats
    Additionla work needs to be done for the opcodes with sizes smaller than 11

--- main.go
+++ main.go
@@ -21,23 +21,31 @@ type Instruction struct {
 
 // Map to associate binary opcodes with their instruction mnemonics and types
 var opcodeMap = map[string]Instruction{
-	"000101":      {"B", "B"},
+	// 6-bit opcodes
+	"000101": {"B", "B"},
+
+	// 8-bit opcodes
+	"10110100": {"CBZ", "CB"},
+	"10110101": {"CBNZ", "CB"},
+
+	// 9-bit opcodes
+	"110100101": {"MOVZ", "IM"},
+	"111100101": {"MOVK", "IM"},
+
+	// 10-bit opcodes
+	"1001000100": {"ADDI", "I"},
+	"1101000100": {"SUBI", "I"},
+
+	// 11-bit opcodes
 	"10001010000": {"AND", "R"},
 	"10001011000": {"ADD", "R"},
-	"1001000100":  {"ADDI", "I"},
 	"10101010000": {"ORR", "R"},
-	"10110100":    {"CBZ", "CB"},
-	"10110101":    {"CBNZ", "CB"},
 	"11001011000": {"SUB", "R"},
-	"1101000100":  {"SUBI", "I"},
-	"110100101":   {"MOVZ", "IM"},
-	"111100101":   {"MOVK", "IM"},
 	"11010011010": {"LSR", "R"},
 	"11010011011": {"LSL", "R"},
 	"11111000000": {"STUR", "D"},
 	"11111000010": {"LDUR", "D"},
 	"11010011100": {"ASR", "R"},
-	"0":           {"NOP", "N/A"},
 	"11101010000": {"EOR", "R"},
 }
 
@@ -64,23 +72,28 @@ func main() {
 }
 
 func defineOpcode(line string) {
+
 	line = strings.ReplaceAll(line, " ", "")
 	// Ensure the line is long enough to contain the opcode
 	if len(line) >= MAXopcodeSize {
 		// Extract the opcode from the line
+
 		opcode := line[:MAXopcodeSize]
 		// Check if the opcode exists in the opcodeMap
 		if inst, exists := opcodeMap[opcode]; exists {
 			// Determine the type of instruction and extract relevant bits
 			switch inst.Type {
 			case "R":
-				// Extract register values for R-type instructions
 				rm := extractBits(line, 11, 15)
 				rn := extractBits(line, 22, 26)
 				rd := extractBits(line, 27, 31)
-				// Print the assembly representation of the instruction
-				fmt.Printf("%s R%d, R%d, R%d\n", inst.Mnemonic, rd, rn, rm)
-				// TODO: Handle other instruction types (I, B, CB, etc.)
+				switch inst.Mnemonic {
+				case "LSR", "LSL":
+					imm := extractBits(line, 16, 21) // Corrected the bit positions
+					fmt.Printf("%s R%d, R%d, #%d\n", inst.Mnemonic, rd, rn, imm)
+				default:
+					fmt.Printf("%s R%d, R%d, R%d\n", inst.Mnemonic, rd, rn, rm)
+				}
 
 			case "I":
 				imm := extractBits(line, 10, 21)
@@ -93,13 +106,27 @@ func defineOpcode(line string) {
 				rt := extractBits(line, 27, 31) // Extract the register to test
 				fmt.Printf("%s R%d, #%d\n", inst.Mnemonic, rt, imm)
 
-				//case "IM":
-				//imm := extrctBits()
+			case "IM":
+				imm := extractBits(line, 9, 24) // Extract the immediate value
+				rd := extractBits(line, 25, 29) // Extract the destination register
+				fmt.Printf("%s R%d, #%d\n", inst.Mnemonic, rd, imm)
 
+			case "D":
+				imm := extractBits(line, 11, 19) // Extract the address offset
+				rn := extractBits(line, 20, 24)  // Extract the base register
+				rt := extractBits(line, 25, 29)  // Extract the source/destination register
+				fmt.Printf("%s R%d, [R%d, #%d]\n", inst.Mnemonic, rt, rn, imm)
+
+			case "B":
+				imm := extractBits(line, 6, 31) // Extract the address offset
+				fmt.Printf("%s #%d\n", inst.Mnemonic, imm)
+
+			case "N/A":
+				fmt.Println("NOP")
 			}
 		} else {
 
-			fmt.Println("Unknown instruction")
+			fmt.Printf("Unknown instruction with opcode: %s\n", opcode)
 		}
 	}
 }
